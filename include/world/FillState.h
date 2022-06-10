cbadd13084f72ba1e538dad24433a4b0a694e995
umby24 <wanderingscripter@gmail.com>
2022-06-09 22:18:24 -0500

    fix some crash bugs

--- include/world/FillState.h
+++ include/world/FillState.h
@@ -12,8 +12,8 @@ namespace D3PP::world {
         std::vector<unsigned char> fillData;
 
         FillState(Common::Vector3S mapSize) { MapSize = mapSize; fillData.resize((mapSize.X * mapSize.Y * mapSize.Z) * 4);}
-        void SetBlock(Common::Vector3S location, unsigned char type) { int index = GetIndex(location.X, location.Y, location.Z); fillData.at(index) = type; }
-        unsigned char GetBlock(Common::Vector3S location) { int index = GetIndex(location.X, location.Y, location.Z); return fillData.at(index); }
+        void SetBlock(Common::Vector3S location, unsigned char type) { int index = GetIndex(location.X, location.Y, location.Z); if (index < fillData.size()) fillData.at(index) = type; }
+        unsigned char GetBlock(Common::Vector3S location) { int index = GetIndex(location.X, location.Y, location.Z); if (index < fillData.size()) return -1; return fillData.at(index); }
     private:
         Common::Vector3S MapSize;
         int GetIndex(int x, int y, int z) {
