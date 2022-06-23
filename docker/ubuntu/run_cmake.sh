aee20438fa463f6574b6c9ffa3f96d2139a6f415
umby24 <wanderingscripter@gmail.com>
2022-06-22 20:10:21 -0500

    working towards fixing the build

--- docker/ubuntu/run_cmake.sh
+++ docker/ubuntu/run_cmake.sh
@@ -4,4 +4,4 @@ set -e
 
 mkdir -p /tmp/D3PP
 cd /tmp/D3PP
-cmake --target D3PP /D3PP
+cmake -DLUA_INCLUDE_DIR=/usr/include/lua5.4 -DLUA_LIBRARY=/usr/lib/x86_64-linux-gnu/liblua5.4.so --target D3PP /D3PP
